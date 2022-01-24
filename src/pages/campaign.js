d62c8f2cd8fc2af5d15bf80d5812398a2ccd9bbd
spark6-dev <dev@spark6.com>
2022-01-24 11:58:04 +0530

    Added campaign page

---
+++ src/pages/campaign.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from 'components/layout/Main/MainLayout'
+import SEO from 'components/seo'
+
+const CampaignPage = ({}) => {
+  return (
+    <Layout>
+      <SEO title="Cake Privacy Policy" />
+      <section className="generic-section">
+        Campaign page
+      </section>
+    </Layout>
+  )
+}
+
+export default CampaignPage
