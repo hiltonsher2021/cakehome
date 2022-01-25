b195566f51ec906dd1a2b4e831db877b7594e444
spark6-dev <dev@spark6.com>
2022-01-25 20:02:22 +0530

    Testimonial section

--- src/components/CampaignCard/CampaignCard.jsx
+++ src/components/CampaignCard/CampaignCard.jsx
@@ -4,15 +4,68 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import * as styles from './CampaignCard.module.scss'
 import RefiTestimonialCard from 'components/RefiTestimonialCard/RefiTestimonialCard'
+import TestimonialCard from 'components/TestimonialCard/TestimonialCard'
+import sectionModel from 'models/Section'
+import { AnchorLink } from 'gatsby-plugin-anchor-links'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const CampaignCard = (data) => {
+  let modeledData
+  let modeledIcons
+  let modeledStarList
+  let image
+  let iconImage
+  let iconsList = []
+  let starList = []
+  let filterData = []
+  let testimonialList = []
+
+  if (data) {
+    debugger
+
+    switch (data?.handle) {
+      case 'campaign':
+        filterData = data?.sectionData.filter((item) => {
+          return item
+        })
+        break
+    }
+    // console.log(filterData, 'filterData')
+
+    // modeledData = sectionModel(filterData[0])
+    // modeledStarList = sectionModel(filterData[2])
+    // modeledIcons = sectionModel(filterData[1])
+    // console.log(modeledStarList, 'modeledStarList')
+    // console.log(iconsList, 'iconsList')
+    // console.log(testimonialList, 'testimonialList')
+    // console.log(starList, 'starList')
+
+    modeledStarList?.sectionReference.filter((item) => {
+      if (item.name === 'Star lists') {
+        starList = item
+      }
+    })
+    modeledIcons?.sectionReference.filter((item) => {
+      if (item?.name === 'Social media icons list') {
+        iconsList = item
+      }
+
+      // else {
+      //   testimonialList = item;
+      // }
+    })
+    // console.log(iconsList, 'iconsList')
+    // console.log(testimonialList, 'testimonialList')
+    // console.log(starList, 'starList')
+  }
+
   var settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    arrows:true,
+    arrows: true,
     responsive: [
       {
         breakpoint: 1200,
@@ -30,36 +83,38 @@ const CampaignCard = (data) => {
     <div className={`${styles.CampaignCard} purchase`}>
       <div className="container">
         <div className="CampaignCard__title-wrap">
+          <h2>{modeledData?.mainTitle}</h2>
+          <div className="CampaignCard__social-wrap">
+            {/* {iconsList?.items.map((item, index) => {
+              iconImage = getImage(item?.image?.gatsbyImageData)
+              return (
+                <AnchorLink key={index}>
+                  <GatsbyImage image={iconImage} alt={item?.image?.title} />
+                </AnchorLink>
+              )
+            })} */}
+          </div>
+        </div>
+
+        <div className="CampaignCard__title-wrap">
           <h2>Title</h2>
         </div>
         <div className="CampaignCard-desktopCards">
           {/* slider section */}
           <Slider {...settings} className="CampaignCard__Wrapper">
             <div className="CampaignCard__item">
-              <RefiTestimonialCard />
+              <TestimonialCard />
             </div>
             <div className="CampaignCard__item">
-              <RefiTestimonialCard />
+              <TestimonialCard />
             </div>
             <div className="CampaignCard__item">
-              <RefiTestimonialCard />
+              <TestimonialCard />
             </div>
             <div className="CampaignCard__item">
-              <RefiTestimonialCard />
+              <TestimonialCard />
             </div>
           </Slider>
-
-          {/* <div className="CampaignCard__Wrapper">
-            <div className="CampaignCard__item">
-              <RefiTestimonialCard />
-            </div>
-            <div className="CampaignCard__item">
-              <RefiTestimonialCard />
-            </div>
-            <div className="CampaignCard__item">
-              <RefiTestimonialCard />
-            </div>
-          </div> */}
         </div>
       </div>
     </div>
---
+++ src/components/TestimonialCard/TestimonialCard.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import * as styles from './TestimonialCard.module.scss'
+import { AnchorLink } from 'gatsby-plugin-anchor-links'
+import { GatsbyImage, getImage, StaticImage } from 'gatsby-plugin-image'
+
+const TestimonialCard = (data) => {
+  let image
+  image = getImage(data?.cardData?.image.gatsbyImageData)
+
+  return (
+    <div className={`${styles.TestimonialCard}`}>
+      <div>
+        <div className="TestimonialCard__top">
+          <div className="TestimonialCard__rating-wrap">
+            {/* {data?.starList?.items.map((item, index) => {
+              let starImage = getImage(item?.image?.gatsbyImageData)
+              return (
+                <GatsbyImage key={index} image={starImage} alt={item?.image?.title} objectFit="contain" />
+              )
+            })} */}
+            <img src="/images/star-green.png" alt="google-icon" />
+
+            <img src="/images/star-green.png" alt="google-icon" />
+
+            <img src="/images/star-green.png" alt="google-icon" />
+
+            <img src="/images/star-green.png" alt="google-icon" />
+
+            <img src="/images/star-green.png" alt="google-icon" />
+          </div>
+          <h3>The entire process was very easy.</h3>
+        </div>
+        <div className="TestimonialCard__middle">
+          <p>
+            Process was remarkably easy and fast. Best mortgage/refi experience
+            we have ever had. No hassles and excellent customer service! We will
+            definitely recommend them to our friends and family. (edited)
+          </p>
+        </div>
+      </div>
+      <div className="TestimonialCard__bottom">
+        <div className="TestimonialCard__footer">
+          <div className="left-side">
+            <h4>Ellie Johnson</h4>
+            <p>Chicago, IL</p>
+          </div>
+          <div className="right-side">
+            <img src="/images/google.png" alt="google-icon" />
+            {/* <AnchorLink href="#">Icon
+              <GatsbyImage image={image} alt={data?.cardData?.image?.title} />
+            </AnchorLink> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TestimonialCard
