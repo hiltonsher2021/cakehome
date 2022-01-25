8efc88b73357b3a010efbe7c31429aab2f82ae3f
spark6-dev <dev@spark6.com>
2022-01-25 12:21:13 +0530

    Form section component added

---
+++ src/components/CampaignForm/CampaignForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as styles from './CampaignForm.module.scss'
+
+const CampaignForm = (data) => {
+  return (
+    <div>
+    Form section
+    </div>
+  )
+}
+
+export default CampaignForm
--- src/pages/campaign.js
+++ src/pages/campaign.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import Layout from 'components/layout/Main/MainLayout'
 import SEO from 'components/seo'
-import CampaignBanner from 'components/CampaignBanner/CampaignBanner';
-import CampaignHeader from 'components/CampaignHeader/CampaignHeader';
+import CampaignBanner from 'components/CampaignBanner/CampaignBanner'
+import CampaignHeader from 'components/CampaignHeader/CampaignHeader'
+import CampaignForm from 'components/CampaignForm/CampaignForm'
 
 const CampaignPage = ({}) => {
-    return ( <
-        Layout >
-        <
-        SEO title = "Campaign" / >
-        <
-        section className = "generic-section" >
-        <
-        CampaignHeader / >
-        <
-        CampaignBanner / >
-        <
-        /section> < /
-        Layout >
-    )
+  return (
+    <Layout>
+      <SEO title="Campaign" />
+      <section className="generic-section">
+        <CampaignHeader />
+        <CampaignBanner />
+        <CampaignForm />
+      </section>
+    </Layout>
+  )
 }
 
 export default CampaignPage
