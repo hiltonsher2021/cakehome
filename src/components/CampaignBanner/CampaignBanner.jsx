7c883f4b98c6f6750d30d0c4448e3c7af11b5a62
spark6-dev <dev@spark6.com>
2022-01-25 10:10:21 +0530

    banner-part campaign

--- src/components/CampaignBanner/CampaignBanner.jsx
+++ src/components/CampaignBanner/CampaignBanner.jsx
@@ -1,35 +1,72 @@
 import React from 'react'
-import Slider from 'react-slick'
 import * as styles from './CampaignBanner.module.scss'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-
 const CampaignBanner = (data) => {
-  var settings = {
-    dots: true,
-    infinite: true,
-    centerMode: true,
-    centerPadding: '28px',
-    responsive: [
-      {
-        breakpoint: 9999,
-        settings: 'unslick',
-      },
-      {
-        breakpoint: 479,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    ],
-  }
+  
   return (
     <div>
-      Banner component
-      {/* Add slider component here */}
-      {/* <Slider
-                {...settings}
-              >1</Slider> */}
+     <section className={styles.CampaginBanner}>
+       <div className="slide-1">
+          <div className="container">
+            <div className="banner__hero">
+              <h1>Your Rate, Your Way.</h1>
+            </div>
+            <div className="banner__content">
+              <h2>You’re just seconds away from getting your amazingly delicious personalized rate!</h2>
+            </div>
+            <div className="banner__form">
+              <div className="banner__form-head">
+              <h3>Let’s get started then!</h3>
+              </div>
+              <div className="banner__form-fields">
+                <input placeholder='First Name' type="text"/>
+                <input placeholder='Last Name' type="text"/>
+              </div>
+            </div>
+            <div className="banner__slider-control">
+                    <div className="banner__next">
+                      <a href="#" title='next'>
+                        <img src="/images/play-icon.svg" alt="slider"/>
+                      </a>
+                    </div>
+                    <div className="banner__slider-dots">
+                      <a className='slider-dots active' href="#" title='dots'></a>
+                      <a className='slider-dots' href="#" title='dots'></a>
+                      <a className='slider-dots' href="#" title='dots'></a>
+                      <a className='slider-dots' href="#" title='dots'></a>
+                      <a className='slider-dots' href="#" title='dots'></a>
+                    </div>
+            </div>
+          </div>
+       </div>
+       <div className="slide-2">
+          <div className="container">
+            <div className="banner__hero">
+              <h1>Great to meet you Name.</h1>
+              <h1 className='d-mob'>How about that FICO score (don't trip, no credit pulls here)</h1>
+              <h2>Your credit rating...</h2>
+            </div>
+            <div className="banner__form">
+              <div className="banner__form-fields">
+              <select name="" id=""></select>
+              </div>
+            </div>
+            <div className="banner__slider-control">
+                    <div className="banner__next">
+                      <a href="#" title='next'>
+                        <img src="/images/play-icon.svg" alt="slider"/>
+                      </a>
+                    </div>
+                    <div className="banner__slider-dots">
+                      <a className='slider-dots' href="#" title='dots'></a>
+                      <a className='slider-dots active' href="#" title='dots'></a>
+                      <a className='slider-dots' href="#" title='dots'></a>
+                      <a className='slider-dots' href="#" title='dots'></a>
+                      <a className='slider-dots' href="#" title='dots'></a>
+                    </div>
+            </div>
+          </div>
+       </div>
+     </section>
     </div>
   )
 }
