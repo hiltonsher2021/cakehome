347f62aab3d078eadbe67e353670717be2790e5e
Michael Osikoya <michael.osikoya@coalitiontechnologies.com>
2022-04-01 01:01:59 +0100

    integrating the sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
-
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
+
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.cakehome.com/",
-    title: "Cake",
-    description: "Cake"
+    siteUrl: 'https://www.cakehome.com/',
+    title: 'Cake',
+    description: 'Cake',
   },
   plugins: [
     {
@@ -14,23 +14,51 @@ module.exports = {
       options: {
         spaceId: `ptoa5hrem9k5`,
         accessToken: process.env.CONTENTFUL_API_KEY,
-        environment: 'develop'
+        environment: 'develop',
       },
     },
     {
       resolve: `gatsby-plugin-sass`,
       options: {
-        implementation: require("node-sass"),
+        implementation: require('node-sass'),
         sassOptions: {
           precision: 6,
         },
       },
     },
     {
-      resolve: "gatsby-plugin-anchor-links",
+      resolve: `gatsby-plugin-anchor-links`,
       options: {
-        offset: -100
-      }
+        offset: -100,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        query: `
+        {
+          allSitePage {
+            nodes {
+              path
+            }
+          }
+        }
+      `,
+        resolveSiteUrl: () => 'https://www.cakehome.com/',
+        resolvePages: ({ allSitePage: { nodes: allPages } }) => {
+          return allPages.map((page) => ({ ...page }))
+        },
+        serialize: ({ path }) => {
+          const theDate = new Date()
+          const month = theDate.getMonth() + 1
+          const day = theDate.getDate()
+          const year = theDate.getFullYear()
+          return {
+            url: path,
+            lastmod: `${month}-${day}-${year}`, //This needs to change
+          }
+        },
+      },
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-root-import`,
@@ -38,4 +66,4 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
   ],
-};
+}
