4e894e230e38ad2e77a1d2322c715e768912c085
Saad Younas <saad.younas@coalitiontechnologies.com>
2022-05-25 18:04:13 +0500

    FAQ Schema

--- src/pages/tools-advice.js
+++ src/pages/tools-advice.js
@@ -12,7 +12,7 @@ import GlossaryBlock from 'components/GlossaryBlock/GlossaryBlock'
 import CheckYourSavings from 'components/CheckYourSavings/CheckYourSavings'
 import SEO from 'components/seo'
 import { initCalculators, unloadCalculators } from 'helpers/calculator'
-
+import { Helmet } from 'react-helmet'
 const isBrowser = typeof window !== 'undefined'
 
 const propTypes = {
@@ -55,6 +55,142 @@ const ToolsAdvice = ({ data }) => {
 
   return (
     <Layout>
+      <Helmet>
+      <script type="application/ld+json">
+        {`{
+          "@context": "https://schema.org",
+          "@type": "FAQPage",
+          "mainEntity": [{
+            "@type": "Question",
+            "name": "What sort of documents are required to refinance?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "W2, 30 Day Paystubs, Mortgage Statement, and Home-Owners Insurance"
+            }
+          },{
+            "@type": "Question",
+            "name": "What out of pocket costs are associated?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "None!"
+            }
+          },{
+            "@type": "Question",
+            "name": "Will my property taxes increase due to an appraisal?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "NO! Your appraisal is strictly to help determine the estimate value of your home for the lender, that value does NOT get sent to your city or county"
+            }
+          },{
+            "@type": "Question",
+            "name": "If I apply, am I committed to refinancing?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "NO! There is no application or appraisal fee so if anything changes, you don’t pay any fees!"
+            }
+          },{
+            "@type": "Question",
+            "name": "Am I going to be taxed on my cash-out?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "NO! Cash out from your equity is your money and does not get taxed!"
+            }
+          },{
+            "@type": "Question",
+            "name": "What does the process look like?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "Three step process, quick application, then approval, and closing."
+            }
+          },{
+            "@type": "Question",
+            "name": "What is an APR?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "APR is an accurate calculation on how much a loan costs. The higher the APR the higher the fees, the lower the APR the lower the fees. Make sure you are getting an APR tailored to how long you plan on keeping your home"
+            }
+          },{
+            "@type": "Question",
+            "name": "Does it cost me anything to get a loan estimate?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "NO! Getting a loan Estimate is totally free, and takes only moments to generate it!"
+            }
+          },{
+            "@type": "Question",
+            "name": "How do I know which interest rate to pick?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "How long you plan on being in the home helps determine which rate is best for you, you should always consult with a Cake councilor to see what your “Break Even” period is with every rate."
+            }
+          },{
+            "@type": "Question",
+            "name": "What is an official loan estimate?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "An official Loan Estimate gives you an itemized breakdown that allows you to simply see your rate, payment, APR, and fees associated with your mortgage based on your qualifications."
+            }
+          },{
+            "@type": "Question",
+            "name": "Does it cost me anything to get a loan estimate?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "NO! Getting a loan Estimate is totally free, and takes only moments to generate it!"
+            }
+          },{
+            "@type": "Question",
+            "name": "Why is my payoff higher than my principal balance?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "Your payoff will include the remaining interest left over on your loan where your principal balance shows what you owed based on your last payment"
+            }
+          },{
+            "@type": "Question",
+            "name": "How do I know which interest rate to pick?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "Pre Paid items are taxes or insurance due ahead of time prior to your loan closing. Your escrow account is where the money is held to pay your upcoming taxes and insurance"
+            }
+          },{
+            "@type": "Question",
+            "name": "Will I have a personal point of contact throughout the process?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "Yes! Your CAKE Concierge will be giving you updates throughout the process and you can easily check online to see the status of your loan"
+            }
+          },{
+            "@type": "Question",
+            "name": "Can I buy a car or finance new purchases during the process?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "Its good practice to not acquire any new debt during a refinance process, given it can jeopardize your loan approval, however you can always check with your CAKE Councilor to confirm."
+            }
+          },{
+            "@type": "Question",
+            "name": "Why is my payoff higher than my principal balance?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "Your payoff will include the remaining interest left over on your loan where your principal balance shows what you owed based on your last payment"
+            }
+          },{
+            "@type": "Question",
+            "name": "What are pre-paid items?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "Pre Paid items are taxes or insurance due ahead of time prior to your loan closing. Your escrow account is where the money is held to pay your upcoming taxes and insurance"
+            }
+          },{
+            "@type": "Question",
+            "name": "Are my closing costs tax-deductible?",
+            "acceptedAnswer": {
+              "@type": "Answer",
+              "text": "Fees can be Tax Deductible such as “points” Make sure you check if your Tax preparer to verify which costs are tax deductible"
+            }
+          }]
+        }`}
+      </script>
+
+      </Helmet>
       <SEO
         title={data?.contentfulPage?.name}
         description={data?.contentfulPage?.description?.description}
