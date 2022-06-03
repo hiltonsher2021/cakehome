87f75f4849439a65fd7cf6976377f5032d3f78f3
Saad Younas <saad.younas@coalitiontechnologies.com>
2022-06-03 17:36:50 +0500

    Fix the Canonical issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
-        siteUrl: `https://www.cakehome.com/`,
+        siteUrl: `https://cakehome.com/`,
         stripQueryString: true,
       },
     },
